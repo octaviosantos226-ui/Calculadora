7e133f0907ddeb11f71607553d81eed8a5f3c5c8
octaviosantos226-ui <octaviosantos226@gmail.com>
2026-04-22 13:54:35 -0300

    feat: implementa funcao multiplicacao

---
+++ Calculadora/src/operacoes/Multiplicacao.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operacoes;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author info1
+ */
+public class Multiplicacao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("digite o primeiro valor");
+        int x = scanner.nextInt();
+        System.out.println("digite o segundo valor");
+        int y = scanner.nextInt();
+        System.out.println("o resultado é:" + (x * y));
+}
+}
